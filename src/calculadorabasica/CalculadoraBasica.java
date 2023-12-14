5b65e8d844474bf4682a3bb0be6b2bee0edd7241
Juan Cruz <jggaretto@gmail.com>
2023-12-14 12:20:52 -0300

    Repositorio CalculadoraBasica

---
+++ src/calculadorabasica/CalculadoraBasica.java
@@ -0,0 +1,16 @@
+
+package calculadorabasica;
+
+import calculadorabasica.igu.Calculadora;
+
+
+public class CalculadoraBasica {
+
+    
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.setVisible(true);
+        calculadora.setLocationRelativeTo(null);
+    }
+    
+}
